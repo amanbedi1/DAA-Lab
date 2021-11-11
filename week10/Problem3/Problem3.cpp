62dc788e84fa25755abc34c74516395f6f4b3435
unknown <Amanpreetsinghbedi_19012722.cse@geu.ac.in>
2021-11-11 19:17:00 +0530

    week10 added

---
+++ week10/Problem3/Problem3.cpp
@@ -0,0 +1,42 @@
+// author : Amanpreet Singh Bedi 
+
+// Problem : Find majority element in array 
+
+#include<bits/stdc++.h>
+
+using namespace std; 
+
+int main(){
+
+	freopen("input.txt","r",stdin); 
+	freopen("output.txt","w",stdout);
+
+	int n; 
+
+	cin>>n; 
+
+	int arr[n]; 
+
+	map<int,int>mp; 
+
+	for(int i=0;i<n;++i){
+		cin>>arr[i]; 
+		mp[arr[i]]++;
+	} 
+
+	string ans="NO";
+
+	for(auto it:mp){
+
+		if(it.second>n/2){
+
+			ans="YES"; 
+			break;
+		}
+ 	}
+
+ 	sort(arr,arr+n); 
+
+ 	cout<<ans<<'\n'; 
+ 	cout<<arr[(n-1)/2]<<'\n'; 
+} 
