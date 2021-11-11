f517d6c1fd9afe15f0934a67b0f4fc98e35806b7
unknown <Amanpreetsinghbedi_19012722.cse@geu.ac.in>
2021-11-11 19:38:07 +0530

    idented correctly

--- week11/Problem1/Problem1.cpp
+++ week11/Problem1/Problem1.cpp
@@ -8,46 +8,46 @@ using namespace std;
 
 int main(){
 
-	freopen("input.txt","r",stdin); 
-	freopen("output.txt","w",stdout);
+    freopen("input.txt","r",stdin); 
+    freopen("output.txt","w",stdout);
 
-	int n; 
+    int n; 
 
-	cin>>n; 
+    cin>>n; 
 
-	int arr[n]; 
+    int arr[n]; 
 
-	for(int i=0;i<n;++i){
-		cin>>arr[i];
-	} 
+    for(int i=0;i<n;++i){
+        cin>>arr[i];
+    } 
 
-	int s; 
-	cin>>s; 
+    int s; 
+    cin>>s; 
 
-	int dp[s+1][n]; 
+    int dp[s+1][n]; 
 
-	for(int i=0;i<n;++i){
-		dp[0][i]=1;
-	}
+    for(int i=0;i<n;++i){
+        dp[0][i]=1;
+    }
 
-	for(int i=1;i<=s;++i){
+    for(int i=1;i<=s;++i){
 
-		for(int j=0;j<n;++j){
+        for(int j=0;j<n;++j){
 
-			dp[i][j]=0;
+            dp[i][j]=0;
 
-			if(i-arr[j]>=0){
+            if(i-arr[j]>=0){
 
-				dp[i][j]+=dp[i-arr[j]][j];
-			} 
+                dp[i][j]+=dp[i-arr[j]][j];
+            } 
             
             if(j>=1){
 
-				dp[i][j]+=dp[i][j-1];
-			} 
-		}
-	} 
+                dp[i][j]+=dp[i][j-1];
+            } 
+        }
+    } 
 
 
-	cout<<dp[s][n-1]<<'\n';
+    cout<<dp[s][n-1]<<'\n';
 }
